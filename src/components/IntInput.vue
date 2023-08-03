eeaa599f7a28d2c9f84d4547765f9510a4016f6d
nekosu <liao.hy@outlook.com>
2023-08-03 11:22:44 +0800

    feat: support auto focus and overflow of roi edit

--- src/components/IntInput.vue
+++ src/components/IntInput.vue
@@ -1,28 +1,60 @@
 <script setup lang="ts">
 import { NInput } from 'naive-ui'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
+
+const props = defineProps<{
+  idx: number
+}>()
+const emits = defineEmits<{
+  overflowInput: [string]
+}>()
 const val = defineModel<number>('value', {
   required: true
 })
 
 const strVal = computed({
   set(v: string | null) {
-    v = v ?? '0'
-    if (v === '') {
+    if (v) {
+      v = v.replace(/^[^\d]*/, '')
+    }
+    if (!v || v === '') {
       v = '0'
     }
-    if (/^(?:[0-9]+)$/.test(v)) {
-      val.value = parseInt(v)
+    const m = /^([0-9]+)(.*)$/.exec(v)
+    if (m) {
+      val.value = parseInt(m[1])
+      if (m[2]) {
+        emits('overflowInput', m[2])
+      }
     }
   },
   get() {
     return `${val.value}`
   }
 })
+
+const el = ref<InstanceType<typeof NInput> | null>(null)
+
+function setValue(v: string | null) {
+  if (v) {
+    v = v.replace(/^[^\d]*/, '')
+  }
+  if (v && v.length > 0) {
+    strVal.value = v
+  } else {
+    el.value?.focus()
+  }
+}
+
+function getIndex() {
+  return props.idx
+}
+
+defineExpose({ setValue, getIndex })
 </script>
 
 <template>
-  <NInput v-model:value="strVal">
+  <NInput ref="el" v-model:value="strVal">
     <template #prefix>
       <slot></slot>
     </template>
--- src/components/RectEdit.vue
+++ src/components/RectEdit.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
 import type { Rect } from '@/types'
 import IntInput from './IntInput.vue'
+import { ref } from 'vue'
 
 const rect = defineModel<Rect>('value', {
   required: true
 })
 const prefixs = ['X:', 'Y:', 'W:', 'H:']
+
+type IntInputType = InstanceType<typeof IntInput>
+const inputEl = ref<IntInputType[]>([])
+function handleOverflow(s: string, i: number) {
+  if (i < 3) {
+    for (const el of inputEl.value) {
+      if (el.getIndex() === i + 1) {
+        el.setValue(s)
+        break
+      }
+    }
+  }
+}
+// 123, 456, 789, 122
 </script>
 
 <template>
@@ -13,10 +28,13 @@ const prefixs = ['X:', 'Y:', 'W:', 'H:']
     <IntInput
       v-for="i in 4"
       :key="i"
+      :idx="i - 1"
+      ref="inputEl"
       :value="rect[i - 1]"
       @update:value="(v: number) => {
         rect[i - 1] = v
       }"
+      @overflow-input="s => handleOverflow(s, i - 1)"
     >
       {{ prefixs[i - 1] }}
     </IntInput>
